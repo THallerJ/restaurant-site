16ed368cd54cc324c4f0da4bf731558fcf4bf4fe
Thomas Haller <hallert60@gmail.com>
2023-06-24 21:15:40 -0500

    added images and icons folder to assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,24 +1,23 @@
-import logo from "./logo.svg";
-import location from "./location.svg";
-import phone from "./phone.svg";
-import { ReactComponent as Twitter } from "./twitter.svg";
-import { ReactComponent as Facebook } from "./facebook.svg";
-import { ReactComponent as Instagram } from "./instagram.svg";
-import customers from "./customers.mp4";
-import food1 from "./food1.jpg";
-import food2 from "./food2.jpg";
-import food3 from "./food3.jpg";
-import food4 from "./food4.jpg";
-import interior from "./interior.jpg";
-import { ReactComponent as DownArrow } from "./down-arrow.svg";
-import chefFire from "./chef-fire.jpg";
-import artist from "./artist.jpg";
-import open from "./open.jpg";
-import { ReactComponent as Close } from "./close.svg";
-import dailySpecial from "./daily-special.jpg";
-import exterior from "./exterior.jpg";
-import seating from "./seating.jpg";
-import eating from "./eating.jpg";
+import logo from "./icons/logo.svg";
+import location from "./icons/location.svg";
+import phone from "./icons/phone.svg";
+import { ReactComponent as Twitter } from "./icons/twitter.svg";
+import { ReactComponent as Facebook } from "./icons/facebook.svg";
+import { ReactComponent as Instagram } from "./icons/instagram.svg";
+import food1 from "./images/food1.jpg";
+import food2 from "./images/food2.jpg";
+import food3 from "./images/food3.jpg";
+import food4 from "./images/food4.jpg";
+import interior from "./images/interior.jpg";
+import { ReactComponent as DownArrow } from "./icons/down-arrow.svg";
+import chefFire from "./images/chef-fire.jpg";
+import artist from "./images/artist.jpg";
+import open from "./images/open.jpg";
+import { ReactComponent as Close } from "./icons/close.svg";
+import dailySpecial from "./images/daily-special.jpg";
+import exterior from "./images/exterior.jpg";
+import seating from "./images/seating.jpg";
+import eating from "./images/eating.jpg";
 
 export {
   logo,
@@ -28,7 +27,6 @@ export {
   Facebook,
   Instagram,
   food1,
-  customers,
   food2,
   food3,
   food4,
